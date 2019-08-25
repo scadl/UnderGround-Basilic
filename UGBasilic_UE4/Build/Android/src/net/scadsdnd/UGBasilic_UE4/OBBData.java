00a8a75b271de2bbdb7d9d00a2678aba2bd835f9
Alexnadr <scad.luncher@gmail.com>
2019-08-25 15:02:10 +0300

    Projects Cleanup & Update RC

---
+++ UGBasilic_UE4/Build/Android/src/net/scadsdnd/UGBasilic_UE4/OBBData.java
@@ -0,0 +1,26 @@
+package net.scadsdnd.UGBasilic_UE4;
+
+public class OBBData
+{
+public static final String AppType = "";
+
+public static class XAPKFile {
+public final boolean mIsMain;
+public final String mFileVersion;
+public final long mFileSize;
+XAPKFile(boolean isMain, String fileVersion, long fileSize) {
+mIsMain = isMain;
+mFileVersion = fileVersion;
+mFileSize = fileSize;
+}
+}
+
+public static final XAPKFile[] xAPKS = {
+new XAPKFile(
+true, // true signifies a main file
+"6", // the version of the APK that the file was uploaded against
+298422594L // the length of the file in bytes
+)
+};
+};
+
